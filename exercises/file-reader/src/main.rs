2550b2fa60f28ed310781104cf233cde264212b2
Michael Tinsley <27818609+michaeltinsley@users.noreply.github.com>
2025-12-29 16:47:47 +0000

    Update main.rs

--- exercises/file-reader/src/main.rs
+++ exercises/file-reader/src/main.rs
@@ -1,26 +1,23 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn read_file(input: &str) {
-    let file = File::open(input);
-    let file = match file {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            std::io::ErrorKind::NotFound => {
-                panic!("File not found: {}", error)
-            }
-            _ => {
-                panic!("Error opening file: {}", error)
-            }
-        },
-    };
+fn read_file(input: &std::path::Path) {
+    let file = File::open(input).unwrap_or_else(|error| {
+        eprintln!(
+            "Error: Failed to open file '{}': {}",
+            input.display(),
+            error
+        );
+        std::process::exit(1);
+    });
 
     let reader = BufReader::new(file);
     for line in reader.lines() {
         match line {
             Ok(line) => println!("{}", line),
             Err(error) => {
-                panic!("Error reading line: {}", error)
+                eprintln!("Error reading from file: {}", error);
+                std::process::exit(1);
             }
         }
     }
@@ -28,11 +25,13 @@ fn read_file(input: &str) {
 
 #[cfg(test)]
 mod tests {
+    use std::path::Path;
+
     use super::*;
 
     #[test]
     fn test_read_file() {
-        let file_path = "test.txt";
+        let file_path = Path::new("test.txt");
         let content = "Hello, world!\nThis is a test file.";
         std::fs::write(file_path, content).unwrap();
 
@@ -53,10 +52,11 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
-    let file_path = args.file.to_str();
+    // let file_path = args.file.to_str();
 
-    match file_path {
-        Some(path) => read_file(path),
-        None => panic!("Invalid file path"),
-    }
+    // match file_path {
+    // Some(path) => read_file(path),
+    // None => panic!("Invalid file path"),
+    // }
+    read_file(&args.file);
 }
